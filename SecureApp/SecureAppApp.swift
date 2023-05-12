71d71ff0635cba4eec40d9994f6328fb887dff08
Javier Etxarri <javieretxarrimarin@gmail.com>
2023-05-12 22:20:04 +0200

    Initial Commit

---
+++ SecureApp/SecureAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  SecureAppApp.swift
+//  SecureApp
+//
+//  Created by Javier Etxarri on 12/5/23.
+//
+
+import SwiftUI
+
+@main
+struct SecureAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
